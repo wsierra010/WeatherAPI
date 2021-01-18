e8c9755c69cbdb51d5ad4a054d0aec11cb9b20c8
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-18 16:31:07 +0100

    current date ok

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 $(".btn_search").on("click", function (e) {
   e.preventDefault();
 
-var city = $("#city").val();
-console.log(city);
+  var city = $("#city").val();
+  console.log(city);
 
-var settings = {
+  var settings = {
     url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84001e7bfdb9a6e78a0b7b03512f2b90&units=metric`,
     method: "GET",
     timeout: 0,
-};
+  };
 
-$.ajax(settings).done(function (response) {
-     // Collect Array Objects API
+  $.ajax(settings).done(function (response) {
+    // Collect Array Objects API
     // console.log(response);
-     // Collect Temperature
+    // Collect Temperature
     // console.log(response.main.temp);
-     // Collect Sunrise
+    // Collect Sunrise
     // console.log(response.sys.sunrise);
-     // Collect Sunset
+    // Collect Sunset
     // console.log(response.sys.sunset);
     getCityName(response.coord.lon, response.coord.lat);
-});
+  });
 
-function getCityName(lon, lat){
+  function getCityName(lon, lat) {
     var settings = {
-        "url": `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=ef8c46d94877be6e25c248a610296d01&units=metric`,
-        "method": "GET",
-        "timeout": 0,
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=ef8c46d94877be6e25c248a610296d01&units=metric`,
+      method: "GET",
+      timeout: 0,
     };
 
     $.ajax(settings).done(function (response) {
-        console.log(response);
-        console.log(response.current.temp);
-        console.log(response.current.sunrise);
-        console.log(response.current.sunset);
+      console.log(response);
+      console.log(response.current.temp);
+      console.log(response.current.sunrise);
+      console.log(response.current.sunset);
     });
+  }
+});
+
+currentDate()
+
+function currentDate() {
+  var currentDate = new Date();
+  var day = currentDate.getDate();
+  var month = currentDate.getMonth();
+  var year = currentDate.getFullYear();
+  console.log(`${day.toString()}/${month.toString() + 1}/${year.toString()}`);
 }
 
-});
+
