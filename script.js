2c67f6808f41e68888323bcd91819e28c180e0bd
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-18 13:18:21 +0100

    city search ok

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var settings = {
-    "url": `http://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=84001e7bfdb9a6e78a0b7b03512f2b90&units=metric`,
-    "method": "GET",
-    "timeout": 0,
-};
+$(".btn_submit").on("click", function (e) {
+  e.preventDefault();
 
-$.ajax(settings).done(function (response) {
+  var city = $("#city").val();
+  console.log(city);
+
+  var settings = {
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84001e7bfdb9a6e78a0b7b03512f2b90&units=metric`,
+    method: "GET",
+    timeout: 0,
+  };
+
+  $.ajax(settings).done(function (response) {
     console.log(response);
-
-
-
-
-
-
+  });
 });
-
