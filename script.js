bfc32b64924aa58ae240be6c7b3319218f72d214
wsierra010 <wsierra010@gmail.com>
2021-01-19 10:00:42 +0100

    Resolve Conflicts

--- script.js
+++ script.js
@@ -1,76 +1,51 @@
 $(".btn_search").on("click", function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  var city = $("#city").val();
-  console.log(city);
+    var city = $("#city").val();
+    console.log(city);
 
-  var settings = {
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84001e7bfdb9a6e78a0b7b03512f2b90&units=metric`,
-    method: "GET",
-    timeout: 0,
-  };
-
-  $.ajax(settings).done(function (response) {
-    // Collect Array Objects API
-    // console.log(response);
-    // Collect Temperature
-    // console.log(response.main.temp);
-    // Collect Sunrise
-    // console.log(response.sys.sunrise);
-    // Collect Sunset
-    // console.log(response.sys.sunset);
-    getCityName(response.coord.lon, response.coord.lat);
-  });
-
-  function getCityName(lon, lat) {
     var settings = {
-<<<<<<< HEAD
-        "url": `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=ef8c46d94877be6e25c248a610296d01&units=metric`,
-        "method": "GET",
-        "timeout": 0,
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84001e7bfdb9a6e78a0b7b03512f2b90&units=metric`,
+        method: "GET",
+        timeout: 0,
     };
 
     $.ajax(settings).done(function (response) {
-        const date = response.current.sunrise;
-        // console.log(d.getTime());
-        const d = new Date(date);
-        console.log(d);
+        // Collect Array Objects API
+        // console.log(response);
+        // Collect Temperature
+        // console.log(response.main.temp);
+        // Collect Sunrise
+        // console.log(response.sys.sunrise);
+        // Collect Sunset
+        // console.log(response.sys.sunset);
+        getCityName(response.coord.lon, response.coord.lat);
+    });
+
+    function getCityName(lon, lat) {
+        var settings = {
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=ef8c46d94877be6e25c248a610296d01&units=metric`,
+        method: "GET",
+        timeout: 0,
+        };
+
+        $.ajax(settings).done(function (response) {
         console.log(response);
         console.log(response.current.temp);
         console.log(response.current.sunrise);
         console.log(response.current.sunset);
-
-        // response.hourly.forEach(e => {
-        //     console.log(e.response.hourly);
-        // });
-    });
-
-}
-});
-=======
-      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=ef8c46d94877be6e25c248a610296d01&units=metric`,
-      method: "GET",
-      timeout: 0,
-    };
-
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      console.log(response.current.temp);
-      console.log(response.current.sunrise);
-      console.log(response.current.sunset);
-    });
-  }
+        });
+    }
 });
 
-currentDate()
+    currentDate()
 
-function currentDate() {
-  var currentDate = new Date();
-  var day = currentDate.getDate();
-  var month = currentDate.getMonth();
-  var year = currentDate.getFullYear();
-  console.log(`${day.toString()}/${month.toString() + 1}/${year.toString()}`);
+    function currentDate() {
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth();
+    var year = currentDate.getFullYear();
+    console.log(`${day.toString()}/${month.toString() + 1}/${year.toString()}`);
 }
 
 
->>>>>>> main
